d5b04b51e5ce74edd59c3f2e549172375cf44559
Eder Gutierrez <gutierrezeder64@gmail.com>
2026-04-10 18:19:29 -0600

    feat: enhance video upload handler to include aspect ratio calculation and update S3 file naming

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -56,20 +56,81 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const videoFilePath = `/tmp/${fileNameWithExt}`;
 
   await Bun.write(videoFilePath, videoFileData);
+  const videoAspectRatio = await getVideoAspectRadio(videoFilePath);
+  const fileNameWithAspectRadio = `${videoAspectRatio}/${fileNameWithExt}`;
+
   const s3VideoFile: S3File = cfg.s3Client.file(
-    fileNameWithExt, {
+    fileNameWithAspectRadio, {
       bucket: cfg.s3BucketName
     });
-  
+
   const videoFileContent = Bun.file(videoFilePath);
   await s3VideoFile.write(videoFileContent, {
     type: mediaType
   });
   
-  const videoFileUrl = `https://${cfg.s3BucketName}.s3.${cfg.s3Region}.amazonaws.com/${fileNameWithExt}`
+  const videoFileUrl = 
+  `https://${cfg.s3BucketName}.s3.${cfg.s3Region}.amazonaws.com/${fileNameWithAspectRadio}`;
+  
   videoMetadata.videoURL = videoFileUrl;
   updateVideo(cfg.db, videoMetadata);
 
   await Bun.file(videoFilePath).delete();
   return respondWithJSON(200, videoMetadata);
+};
+
+export async function getVideoAspectRadio(filePath: string) {
+
+  const videoFile = Bun.file(filePath);
+  if (videoFile.type !== "video/mp4") {
+    throw new BadRequestError("Video should be an image file");
+  };
+
+  const proc = Bun.spawn({
+    cmd: [
+      "ffprobe",
+      "-v",
+      "error",
+      "-select_streams",
+      "v:0",
+      "-show_entries",
+      "stream=width,height",
+      "-of",
+      "json",
+      filePath
+    ],
+    stdout:"pipe"
+  });
+  const stdoutText = await new Response(proc.stdout).text(); 
+  const stderrText = await new Response(proc.stderr).text();
+  const exited = await proc.exited;
+
+  if (exited !== 0 || stderrText) {
+    throw new Error("Error when trying to get the width and height of the video file");
+  };
+
+  interface procResponse {
+    programs: [];
+    streams: [
+        {
+            width: number;
+            height: number;
+        }
+    ];
+  };
+
+  const procParsed: procResponse = JSON.parse(stdoutText);
+  const videoWidth = procParsed.streams[0].width;
+  const videoHeight = procParsed.streams[0].height;
+  
+  const videoRatio = Math.floor(videoWidth / videoHeight); 
+
+  switch (videoRatio) {
+    case 0:
+      return "portrait";
+    case 1:
+      return "landscape";
+    default:
+      return "other";
+  };
 };
